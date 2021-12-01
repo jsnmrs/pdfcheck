bf9164c18f590343581b385fb814c29c700b5fd5
Jason Morris <jasonmorris@gmail.com>
2021-12-01 08:25:46 -0500

    Auto-fixes from rule changes

--- pdfcheck.js
+++ pdfcheck.js
@@ -1,12 +1,12 @@
-(function pdfCheck () {
+(function pdfCheck() {
   "use strict";
 
   var form = document.querySelector("#dropzone"),
-    input = form.querySelector("input[type=\"file\"]"),
+    input = form.querySelector('input[type="file"]'),
     label = form.querySelector("label"),
     droppedFiles = false;
 
-  input.addEventListener("change", function processStart (e) {
+  input.addEventListener("change", function processStart(e) {
     showFiles(e.target.files);
     processFiles(e.target.files);
   });
@@ -27,7 +27,7 @@
     form.addEventListener("dragend", dragOut, false);
     form.addEventListener("drop", dragOut, false);
 
-    form.addEventListener("drop", function fileDrop (e) {
+    form.addEventListener("drop", function fileDrop(e) {
       droppedFiles = e.dataTransfer.files;
       showFiles(droppedFiles);
       processFiles(droppedFiles);
@@ -35,37 +35,37 @@
   }
 
   // Firefox focus bug fix for file input
-  input.addEventListener("focus", function fileFocus () {
+  input.addEventListener("focus", function fileFocus() {
     input.classList.add("has-focus");
   });
 
-  input.addEventListener("blur", function fileBlur () {
+  input.addEventListener("blur", function fileBlur() {
     input.classList.remove("has-focus");
   });
 
-  function dragIn () {
+  function dragIn() {
     form.classList.add("is-dragover");
   }
 
-  function dragOut () {
+  function dragOut() {
     form.classList.remove("is-dragover");
   }
 
-  function stopShort (event) {
+  function stopShort(event) {
     event.preventDefault();
     event.stopPropagation();
   }
 
-  function showFiles (files) {
+  function showFiles(files) {
     label.textContent = files[0].name;
     if (files.length > 1) {
-      label.textContent = input.
-        getAttribute("data-multiple-caption").
-        replace("{count}", files.length);
+      label.textContent = input
+        .getAttribute("data-multiple-caption")
+        .replace("{count}", files.length);
     }
   }
 
-  function addFlag (className, innerHTML) {
+  function addFlag(className, innerHTML) {
     var tempNode = document.createElement("p");
 
     tempNode.className = "flag " + className;
@@ -74,7 +74,7 @@
   }
 
   // Check if valid PDF file (read first 8 bytes, match regex)
-  function validatePDF (fileData) {
+  function validatePDF(fileData) {
     var markup,
       dataHeader = fileData.substr(0, 8),
       regexHeader = /%PDF-(1.[0-7])/g,
@@ -94,26 +94,26 @@
   }
 
   // Check if StructTreeRoot is set and count tags
-  function findTags (fileData) {
+  function findTags(fileData) {
     var markup,
       regexTree = /StructTreeRoot\s(\d*)\s(\d*)/g,
       matchTree = regexTree.exec(fileData);
 
     if (matchTree) {
       markup =
-        "<span>Tagged <a href=\"#help-tagged\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>Yes (" +
+        '<span>Tagged <a href="#help-tagged" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>Yes (' +
         matchTree[1] +
         " tags)</strong>";
       addFlag("success", markup);
     } else {
       markup =
-        "<span>Tagged <a href=\"#help-tagged\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>No</strong>";
+        '<span>Tagged <a href="#help-tagged" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>No</strong>';
       addFlag("failure", markup);
     }
   }
 
   // Check if Lang is set, display value if set
-  function findLang (fileData) {
+  function findLang(fileData) {
     var markup,
       regexLang = /Lang((<|\()\S*(>|\)))/g,
       matchLang = regexLang.exec(fileData);
@@ -124,19 +124,19 @@
         matchLang[1] = "(en-US)";
       }
       markup =
-        "<span>Language <a href=\"#help-language\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>" +
+        '<span>Language <a href="#help-language" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>' +
         matchLang[1] +
         "</strong>";
       addFlag("success", markup);
     } else {
       markup =
-        "<span>Language <a href=\"#help-language\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>not set</strong>";
+        '<span>Language <a href="#help-language" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>not set</strong>';
       addFlag("failure", markup);
     }
   }
 
   // Check MarkInfo exists and whether true or false
-  function findMark (fileData) {
+  function findMark(fileData) {
     var markup,
       regexMarked = /<<\/Marked (true|false)/g,
       matchMarked = regexMarked.exec(fileData);
@@ -144,41 +144,42 @@
     if (matchMarked) {
       if (matchMarked[1] === "true") {
         markup =
-          "<span>Marked <a href=\"#help-marked\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>True</strong>";
+          '<span>Marked <a href="#help-marked" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>True</strong>';
         addFlag("success", markup);
       } else {
         markup =
-          "<span>Marked <a href=\"#help-marked\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>False</strong>";
+          '<span>Marked <a href="#help-marked" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>False</strong>';
         addFlag("warning", markup);
       }
     } else {
       markup =
-        "<span>Marked <a href=\"#help-marked\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>No</strong>";
+        '<span>Marked <a href="#help-marked" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>No</strong>';
       addFlag("failure", markup);
     }
   }
 
   // Check for PDF/UA identifier
-  function findUA (fileData) {
+  function findUA(fileData) {
     var markup,
       regexPDFUA = /<pdfaSchema:prefix>pdfuaid<\/pdfaSchema:prefix>/g,
       matchPDFUA = regexPDFUA.exec(fileData);
 
     if (matchPDFUA) {
       markup =
-        "<span>PDF/UA identifier <a href=\"#help-pdfua\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>Yes</strong>";
+        '<span>PDF/UA identifier <a href="#help-pdfua" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>Yes</strong>';
       addFlag("success", markup);
     } else {
       markup =
-        "<span>PDF/UA identifier <a href=\"#help-pdfua\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>Not set</strong>";
+        '<span>PDF/UA identifier <a href="#help-pdfua" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>Not set</strong>';
       addFlag("warning", markup);
     }
   }
 
   // Check for DisplayDocTitle and dc:title
-  function findTitle (fileData) {
+  function findTitle(fileData) {
     var markup,
-      regexTitle = /<dc:title>[\s\S]*?<rdf:Alt>([\s\S]*?)<\/rdf:Alt>[\s\S]*?<\/dc:title>/g,
+      regexTitle =
+        /<dc:title>[\s\S]*?<rdf:Alt>([\s\S]*?)<\/rdf:Alt>[\s\S]*?<\/dc:title>/g,
       matchTitle = regexTitle.exec(fileData),
       emptyTag = /<rdf:li xml:lang="x-default"\/>/g,
       matchEmpty = emptyTag.exec(matchTitle);
@@ -186,28 +187,27 @@
     if (matchTitle) {
       if (matchEmpty) {
         markup =
-          "<span>Document Title <a href=\"#help-title\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>Empty</strong>";
+          '<span>Document Title <a href="#help-title" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>Empty</strong>';
         addFlag("warning", markup);
       } else {
         markup =
-          "<span>Document Title <a href=\"#help-title\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>" +
+          '<span>Document Title <a href="#help-title" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>' +
           matchTitle[1] +
           "</strong>";
         addFlag("default", markup);
       }
     } else {
       markup =
-        "<span>Document Title <a href=\"#help-title\" class=\"more-info\" aria-label=\"more information on this check\" title=\"more information on this check\">i</a></span> <strong>Not set</strong>";
+        '<span>Document Title <a href="#help-title" class="more-info" aria-label="more information on this check" title="more information on this check">i</a></span> <strong>Not set</strong>';
       addFlag("failure", markup);
     }
   }
 
   // Build file heading - ex: 1. document.pdf [PDF - 236 KB]
-  function buildHeading (file, fileNumber) {
+  function buildHeading(file, fileNumber) {
     var fileLabel,
       markup,
-      fileExt = file.name.split(".").pop().
-        toUpperCase(),
+      fileExt = file.name.split(".").pop().toUpperCase(),
       fileSize = file.size / 1024 / 1024,
       fileSizeSuffix = " MB";
 
@@ -222,7 +222,7 @@
     addFlag("title", markup);
   }
 
-  function runCheck (file, fileData, fileNumber) {
+  function runCheck(file, fileData, fileNumber) {
     var valid;
 
     buildHeading(file, fileNumber);
@@ -237,17 +237,17 @@
     }
   }
 
-  function readMultiFiles (files) {
+  function readMultiFiles(files) {
     var reader = new FileReader();
 
-    function readFile (index) {
+    function readFile(index) {
       var file;
 
       if (index >= files.length) {
         return;
       }
       file = files[index];
-      reader.onload = function onload (e) {
+      reader.onload = function onload(e) {
         var fileData = e.target.result;
 
         runCheck(file, fileData, index + 1);
@@ -258,12 +258,12 @@
     readFile(0);
   }
 
-  function processFiles (files) {
+  function processFiles(files) {
     document.getElementById("report").innerHTML = "";
     readMultiFiles(files);
   }
 
-  function isAdvancedUpload () {
+  function isAdvancedUpload() {
     var div = document.createElement("div");
 
     return (
@@ -272,4 +272,4 @@
       "FileReader" in window
     );
   }
-}());
+})();
